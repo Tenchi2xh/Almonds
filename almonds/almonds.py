a191db5ab11ac2d7ecb0cd095776595f64568512
Tenchi <tenchi@team2xh.net>
2015-10-30 12:33:28 +0100

    exit print fix

--- almonds/almonds.py
+++ almonds/almonds.py
@@ -520,7 +520,7 @@ def main(p):
 
     spent = (time.time() - begin) // 60
     spaces = " " * 26
-    print(symbols.encode("\n".join(splash)))
+    print("\n".join(splash))
     print("%s%d minute%s exploring fractals, see you soon :)\n" % (spaces, spent, "s" if spent > 1 else ""))
     print("%s- Almonds %s by Tenchi <tenchi@team2xh.net>\n" % (spaces, __version__))
 
